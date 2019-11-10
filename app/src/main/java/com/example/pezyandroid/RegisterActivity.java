4e9bb356dc2a27cf5d0f9caaae80aa7dde08211b
Wesarut Khm <anubis.smile@gmail.com>
2019-11-10 17:55:13 +0700

    Minor - Improvement - Write register activity design.

---
+++ app/src/main/java/com/example/pezyandroid/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.pezyandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        bindObj();
+    }
+
+    private void bindObj() {
+
+    }
+}
