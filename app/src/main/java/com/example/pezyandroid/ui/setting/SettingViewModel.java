de2d980f28f15a0fddd790326f0306b99e6085c0
Wesarut Khm <anubis.smile@gmail.com>
2019-11-19 11:17:31 +0700

    Minor - Improvement - Adding menu for bottom tab menu.

--- app/src/main/java/com/example/pezyandroid/ui/setting/SettingViewModel.java
+++ app/src/main/java/com/example/pezyandroid/ui/setting/SettingViewModel.java
@@ -1,16 +1,16 @@
-package com.example.pezyandroid.ui.dashboard;
+package com.example.pezyandroid.ui.setting;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class SettingViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public SettingViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is chat fragment");
+        mText.setValue("This is setting fragment");
     }
 
     public LiveData<String> getText() {
