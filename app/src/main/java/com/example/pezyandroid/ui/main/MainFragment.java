780d4fd64e853aa4300d119665cc001b2cac853f
Wesarut Khm <anubis.smile@gmail.com>
2019-11-22 13:19:42 +0700

    Minor - Improvement - Creating slide show by PlutoView.

--- app/src/main/java/com/example/pezyandroid/ui/main/MainFragment.java
+++ app/src/main/java/com/example/pezyandroid/ui/main/MainFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pezyandroid.R;
 import com.example.pezyandroid.databinding.FragmentNewMainNavBinding;
+import com.opensooq.pluto.PlutoView;
 import com.opensooq.pluto.base.PlutoAdapter;
 import com.opensooq.pluto.base.PlutoViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
 
@@ -21,17 +27,58 @@ public class MainFragment extends Fragment {
 
     private FragmentNewMainNavBinding fBind;
 
+    private PlutoView slide;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mainViewModel =
                 ViewModelProviders.of(this).get(MainViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_new_main_nav, container, false);
 
         fBind = FragmentNewMainNavBinding.inflate(inflater, container, false);
 
-
+        slideShow();
 
         return fBind.getRoot();
+    }
+
+    private void slideShow() {
+        List<Integer> imgs = new ArrayList<>();
+        imgs.add(R.drawable.slide_banner);
+        imgs.add(R.drawable.slide_banner);
+        imgs.add(R.drawable.slide_banner);
+        imgs.add(R.drawable.slide_banner);
+        imgs.add(R.drawable.slide_banner);
+        imgs.add(R.drawable.slide_banner);
+
+        SlideAdapter adapter = new SlideAdapter(imgs);
+        fBind.sliderView.create(adapter, getLifecycle());
+    }
+
+    private class SlideAdapter extends PlutoAdapter<Integer, SlideAdapter.SlideViewHolder>{
+
+        public SlideAdapter(List<Integer> items){
+            super(items);
+        }
+
+        @Override
+        public SlideViewHolder getViewHolder(ViewGroup viewGroup, int i) {
+            return new SlideViewHolder(viewGroup, R.layout.layout_slide_show);
+        }
+
+        private class SlideViewHolder extends PlutoViewHolder<Integer>{
+
+            private ImageView img;
+
+            public SlideViewHolder(ViewGroup parent, int layout){
+                super(parent, layout);
+                img = getView(R.id.imgSlide);
+            }
+
+            @Override
+            public void set(Integer integer, int i) {
+                img.setImageResource(integer);
+            }
+        }
     }
 
 }
