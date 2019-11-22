4dfdbdb41a7cda9a97f863bdc64d4d739a7455b7
Wesarut Khm <anubis.smile@gmail.com>
2019-11-22 12:21:44 +0700

    Minor - Improvement - Redesign main activity.

--- app/src/main/java/com/example/pezyandroid/MainNavActivity.java
+++ app/src/main/java/com/example/pezyandroid/MainNavActivity.java
@@ -1,10 +1,15 @@
 package com.example.pezyandroid;
 
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,12 +17,16 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainNavActivity extends AppCompatActivity {
 
+    private EditText edt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main_nav);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
--- app/src/main/java/com/example/pezyandroid/ui/main/MainFragment.java
+++ app/src/main/java/com/example/pezyandroid/ui/main/MainFragment.java
@@ -8,19 +8,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pezyandroid.R;
+import com.example.pezyandroid.databinding.FragmentNewMainNavBinding;
+import com.opensooq.pluto.base.PlutoAdapter;
+import com.opensooq.pluto.base.PlutoViewHolder;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mainViewModel;
 
+    private FragmentNewMainNavBinding fBind;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mainViewModel =
                 ViewModelProviders.of(this).get(MainViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+//        View root = inflater.inflate(R.layout.fragment_new_main_nav, container, false);
 
-        return root;
+        fBind = FragmentNewMainNavBinding.inflate(inflater, container, false);
+
+
+
+        return fBind.getRoot();
     }
+
 }
