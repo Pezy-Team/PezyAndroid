4dd4efd175ce8531b98c57693a75b9670de3f97b
Wesarut Khm <anubis.smile@gmail.com>
2019-12-10 20:04:04 +0700

    Minor - Improvement - Create store affimation activity.

---
+++ app/src/main/java/com/example/pezyandroid/StoreAffirmationActivity.java
@@ -0,0 +1,37 @@
+package com.example.pezyandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pezyandroid.databinding.ActivityStoreAffirmationBinding;
+
+public class StoreAffirmationActivity extends AppCompatActivity {
+
+    private ActivityStoreAffirmationBinding fBind;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_affirmation);
+
+        fBind = DataBindingUtil.setContentView(StoreAffirmationActivity.this, R.layout.activity_store_affirmation);
+
+        onConfirmClick();
+
+    }
+
+    private void onConfirmClick() {
+        fBind.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StoreAffirmationActivity.this, null);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
